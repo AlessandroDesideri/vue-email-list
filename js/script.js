7a3df5ffe47b6bce3280474725094bdc4c886a8c
Alessandro <alessandro01desideri@gmail.com>
2022-01-31 10:03:34 +0100

    Completamento esercizio

---
+++ js/script.js
@@ -0,0 +1,25 @@
+new Vue({
+    el: "#app",
+    data: {
+        emailList: [
+
+        ]
+    },
+    methods: {
+        getEmail() {
+            axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then((email) => {
+                // verificare nessuna ripetizione delle email
+                if (!this.emailList.includes(email.data.response)) {
+                    this.emailList.push(email.data.response);
+                } else {
+                    this.getEmail();
+                }
+            })
+        }
+    },
+    mounted() {
+        for (let i = 0; i < 10; i++) {
+            this.getEmail();
+        }
+    }
+})
